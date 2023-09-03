52ba1704a9edc6f2e6de8f791ecd3786992be698
kit0-0 <kt1767@gmail.com>
2023-09-04 04:23:38 +0530

    fix linters

--- book.rb
+++ book.rb
@@ -1,17 +1,16 @@
 class Book
-    attr_accessor :title, :author
-    attr_reader :rentals
-  
-    def initialize(title, author)
-      @title = title
-      @author = author
-      @rentals = []
-    end
-  
-    def add_rental(person, date)
-      rental = Rental.new(self, person, date)
-      rentals << rental
-      person.add_rental(rental)
-    end
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
   end
-  
+
+  def add_rental(person, date)
+    rental = Rental.new(self, person, date)
+    rentals << rental
+    person.add_rental(rental)
+  end
+end
