ab85d06eabf8c15b85d0a00a50388a2f42790ed5
kit0-0 <kt1767@gmail.com>
2023-09-04 17:29:00 +0530

    fix linters

--- book.rb
+++ book.rb
@@ -8,7 +8,6 @@ class Book
   end
 
   def add_rental(date, person)
-    @rentals.push(Rental.new(date, self, person)) 
-    unless @rentals.include?(Rental.new(date, self, person))
+    @rentals.push(Rental.new(date, self, person)) unless @rentals.include?(Rental.new(date, self, person))
   end
 end
